44217a76e7dae198983d3fdaeff505b8a0e9f535
Neucrack <CZD666666@gmail.com>
2016-04-23 23:53:06 +0800

    add interface for socket

--- demo/embedded/lib/inc/socket.h
+++ demo/embedded/lib/inc/socket.h
@@ -2,14 +2,14 @@
 #define __SOCKET_H
 #include "stdbool.h"
 
+enum Socket_Type{Socket_Stream,Socket_Dgram};//TCP & UDP
+enum Socket_Protocol{Socket_Protocol_IPV4,Socket_Protocol_IPV6};
+	
 class Socket
 {
 public :
-	enum Socket_Type{Socket_Stream,Socket_Dgram};
-	enum Socket_Protocol{Socket_Protocol_IPV4,Socket_Protocol_IPV6};
-	
 	Socket(){}
-	virtual bool connect(long int ipAddr,short int port,Socket_Type socketType,Socket_Protocol socketProtocol) = 0;
+	virtual bool Connect(char* ipAddr,short int port,Socket_Type socketType,Socket_Protocol socketProtocol=Socket_Protocol_IPV4) = 0;
 	virtual bool Write(char* data,unsigned int num) = 0;
 	virtual unsigned int Read(char* data) = 0;
 	virtual bool IsAlive() = 0;

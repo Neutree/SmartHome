d11e05072d567f4bc4a4f395e3a737c5ea084552
Neucrack <CZD666666@gmail.com>
2016-05-07 23:03:42 +0800

    登录、链路保持测试通过

--- demo/server/src/com/neucrack/communication/ToDevices.java
+++ demo/server/src/com/neucrack/communication/ToDevices.java
@@ -2,6 +2,10 @@ package com.neucrack.communication;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+
+import com.neucrack.devices.*;
+import com.neucrack.tool.CRC;
 
 public class ToDevices {
 	private DataInputStream mInStream=null;
@@ -12,9 +16,77 @@ public class ToDevices {
 		mOutStream = outStream;
 	}
 	
-	public SignInfo WaitSignIn(){
-		SignInfo result = new SignInfo();
-		return result;
+	public boolean WaitSignIn(SignInfo signIn){
+		byte[] mDeviceNumber = new byte[6];
+		byte[] mUserName = new byte[11];
+		byte[] mUserPasswd = new byte[16];
+		
+		byte dataToRead[] = new byte[512] ;
+		try {
+			int size = mInStream.read(dataToRead);
+			if(size>0){
+				//帧头
+				if(((short)dataToRead[0]&0xff)!=0xab || ((short)dataToRead[1]&0xff)!=0xac)
+					return false;
+				int datalength = (short)dataToRead[17]<<8|dataToRead[18];
+				//CRC校验
+				int parity = CRC.CRC16Calculate(dataToRead,datalength+19);
+				int parity2 = (dataToRead[19+datalength]<<8|dataToRead[20+datalength])&0xffff;
+				if(parity != parity2)
+					return false;
+				//校验成功
+				if(dataToRead[4]!=1)//不是是请求登录
+					return false;
+				System.arraycopy(dataToRead, 5, mDeviceNumber, 0, 6);
+				System.arraycopy(dataToRead, 19, mUserName, 0, 11);
+				System.arraycopy(dataToRead, 30, mUserPasswd, 0, 16);
+				signIn.device = Byte6ToMac(mDeviceNumber);
+				signIn.userName = BytesToString(mUserName,11);
+				signIn.userPasswd = MD5_32BytesToString(mUserPasswd);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+	public boolean KeepAlive(String device){
+		byte[] data = new byte[50];
+		byte[] deviceNumber = MacToBytes(device);
+		data[0] = (byte) 0xab;
+		data[1] = (byte) 0xac;
+		data[2] = (byte) 0x00;
+		data[3] = (byte) 0x10;
+		data[4] = (byte) 0x01;
+		System.arraycopy(deviceNumber, 0, data, 5, 6);
+		data[17] = (byte) 0x00;
+		data[18] = (byte) 0x01;
+		data[19] = (byte) 0x01;
+		long crc16 = CRC.CRC16Calculate(data, 20);
+		data[20] = (byte) (crc16>>8&0xff);
+		data[21] = (byte) (crc16&0xff);
+		try {
+			mOutStream.write(data, 0, 22);;
+			int size = mInStream.read(data);
+			if(size>0){
+				if(!VerifyFrame(data))
+					return false;
+				//校验成功
+				if(data[4]!=2)//不是是响应链路保持
+					return false;
+				if(data[19]==1){//设备
+					
+				}
+				else if(data[20]==2){//手机
+					
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	public boolean LightControl(String device,boolean isOn){
 		return isOn;
@@ -25,35 +97,63 @@ public class ToDevices {
 	public boolean DoorControl(String device,boolean isOn){
 		return isOn;
 	}
-	public boolean GetDoorStatus(String device,DoorInfo door){
+	public boolean GetDoorStatus(String device,Door door){
 		return false;
 	}
 	
-	public boolean GetLightStatus(String device,LightInfo light){
+	public boolean GetLightStatus(String device,Light light){
 		return false;
 	}
-	public boolean GetCurtainStatus(String device,CurtainInfo curtain){
+	public boolean GetCurtainStatus(String device,Curtain curtain){
 		return false;
 	}
-	public boolean GetSensorData(String device,int SensorName,SensorInfo sensor){
+	public boolean GetSensorData(String device,int SensorName,Sensor sensor){
 		return false;
 	}
 	
-	class SignInfo{
-		public String device;
-		public String userName;
-		public String passwd;
+	private boolean VerifyFrame(byte[] data){
+		//帧头
+		if(((short)data[0]&0xff)!=0xab || ((short)data[1]&0xff)!=0xac)
+			return false;
+		int datalength = (short)data[17]<<8|data[18];
+		//CRC校验
+		int parity = CRC.CRC16Calculate(data,datalength+19);
+		int parity2 = (data[19+datalength]<<8|data[20+datalength])&0xffff;
+		if(parity != parity2)
+			return false;
+		return true;
 	}
-	class LightInfo{
-		public boolean isOn;
-	}
-	class CurtainInfo{
-		public boolean isOn;
-	}
-	class SensorInfo{
-		public int value;
-	}
-	class DoorInfo{
-		public boolean isOn;
-	}
+	 private static String Byte6ToMac(byte[] c) {
+	        return Integer.toHexString(((int)c[0]&0xff))+":"+Integer.toHexString(((int)c[1]&0xff))+":"+Integer.toHexString(((int)c[2]&0xff))+":"
+	        		+((short)c[3]&0xff)+":"+Integer.toHexString(((int)c[4]&0xff))+":"+Integer.toHexString(((int)c[5]&0xff)); 
+	 }
+	 
+	 private static byte[] MacToBytes(String mac) {
+		byte []macBytes = new byte[6];
+		String [] strArr = mac.split(":"); 
+		for(int i = 0;i < strArr.length; i++){
+			int value = Integer.parseInt(strArr[i],16);
+			macBytes[i] = (byte) value;
+		}
+		return macBytes;
+	 }
+	 
+	 private static String BytesToString(byte[] b,int size){
+		 String str= "";
+		 for(int i=0;i<size;++i){
+			 str+=b[i]-'0';
+		 }
+		 return str;
+	 }
+	 private static String MD5_32BytesToString(byte[] md5){
+		 String r="";
+		 String temp=null;
+		 for(int i=0;i<16;++i){
+			 temp = Integer.toHexString(((int)md5[i]&0xff));
+			 if(temp.length()<2)
+				 temp = "0"+temp;
+			 r += temp;
+		 }
+		 return r;
+	 }
 }
--- demo/server/src/com/neucrack/server/ServerThead.java
+++ demo/server/src/com/neucrack/server/ServerThead.java
@@ -9,10 +9,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
 import java.sql.Time;
 
 import org.omg.CORBA.DATA_CONVERSION;
 
+import com.neucrack.communication.ToDevices;
+import com.neucrack.devices.*;
 import com.neucrack.tool.CRC;
 import com.neucrack.tool.Date_TimeStamp;
 
@@ -26,10 +29,9 @@ public class ServerThead extends Thread {
 	protected DataInputStream mInStream=null;
 	protected DataOutputStream mOutStream=null;
 	
-	public byte mUserName[]= new byte[11];
-	public byte[] mDeviceNumber=new byte[6];
-	public byte[] mUserPasswd  = new byte[16];
-	public byte mDeviceType;
+	public SignInfo mSignInfo = new SignInfo();
+	
+	public ToDevices mToDevices;
 	
 	private boolean mIsSginIn=false;
 
@@ -45,7 +47,7 @@ public class ServerThead extends Thread {
 		if(!getIn_Out_stream())
 		{
 			System.out.println("获取输入输出流错误");
-			close();
+			Close();
 			return;
 		}
 		
@@ -54,9 +56,15 @@ public class ServerThead extends Thread {
 			
 			//登录检测
 			if(!mIsSginIn){
+				try {
+					socket.setSoTimeout(20000);
+				} catch (SocketException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				//等待登录请求
-				if(WaitForSignIn()){
-					System.out.println("设备登录请求到达，设备号："+Byte6ToMac(mDeviceNumber)+"用户名："+BytesToString(mUserName,11));
+				if(mToDevices.WaitSignIn(mSignInfo)){
+					System.out.println("设备登录请求到达，设备号："+mSignInfo.device+"用户名："+mSignInfo.userName);
 					//用户和设备验证
 					if(!SignInVerify()){//验证失败
 						System.out.println("登录验证失败！！！！");
@@ -64,26 +72,33 @@ public class ServerThead extends Thread {
 					}
 					System.out.println("设备登录成功!!");
 					mIsSginIn = true;
-					break;
+					try {
+						socket.setSoTimeout(5000);
+					} catch (SocketException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					continue;
 				}
-				System.out.println("fail");
 				if(Date_TimeStamp.timeStamp()-startTime>20000){//20s还没有登录则关闭连接
 					System.out.println("等待登录请求超时");
-					close();
+					Close();
 				}
 				continue;
 			}
 			
-			//心跳保持包
-			if(Date_TimeStamp.timeStamp()-keepAliveTime==5){
+			//心跳保持包,5分钟一次
+			if(Date_TimeStamp.timeStamp()-keepAliveTime>=300000){
 				keepAliveTime = Date_TimeStamp.timeStamp();
-				if(!KeepAlive()){
+				if(!mToDevices.KeepAlive(mSignInfo.device)){
 					System.out.println("心跳保持失败");
-					if(!KeepAlive()){
+					if(!mToDevices.KeepAlive(mSignInfo.device)){
 						System.out.println("第二次心跳保持失败!失去连接！！！！！！");
+						Close();
 						break;
 					}
 				}
+				System.out.println("链路保持成功！");
 			}
 			
 			
@@ -108,38 +123,11 @@ public class ServerThead extends Thread {
 			e.printStackTrace();
 			return false;
 		}
+		
+		mToDevices = new ToDevices(mInStream,mOutStream);
 		return true;
 	}
-	//等待设备登录
-	private boolean WaitForSignIn(){
-		byte dataToRead[] = new byte[1024] ;
-		try {
-			int size = mInStream.read(dataToRead);
-			if(size>0){
-				//帧头
-				if(((short)dataToRead[0]&0xff)!=0xab || ((short)dataToRead[1]&0xff)!=0xac)
-					return false;
-				int datalength = (short)dataToRead[17]<<8|dataToRead[18];
-				//CRC校验
-				int parity = CRC.CRC16Calculate(dataToRead,datalength+19);
-				int parity2 = (dataToRead[19+datalength]<<8|dataToRead[20+datalength])&0xffff;
-				if(parity != parity2)
-					return false;
-				//校验成功
-				if(dataToRead[4]==1){
-					System.arraycopy(dataToRead, 5, mDeviceNumber, 0, 6);
-					System.arraycopy(dataToRead, 19, mUserName, 0, 11);
-					System.arraycopy(dataToRead, 30, mUserPasswd, 0, 16);
-					mDeviceType = (byte) dataToRead[46];
-				}
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return true;
-	}
-	
+
 	
 	//验证设备和用户
 	private boolean SignInVerify() {
@@ -147,29 +135,10 @@ public class ServerThead extends Thread {
 		return true;
 	}
 	
-	//链路保持
-	private boolean KeepAlive(){
-		return true;
-	}
 
-	public void deal() {
 
-	}
 
-	protected void write() {
-//		try {
-//			
-//			pw.write(backMsg);
-//			pw.flush();// 调用flush()方法将缓冲输出
-//			System.out.println("Info ， Socket send :" + backMsg);
-//		} catch (Exception e) {
-//			System.out.println("Abnormal , Socket : 写入数据异常 ：");
-//			e.printStackTrace();
-//		}
-
-	}
-
-	protected void close() {
+	protected void Close() {
 		try {
 			if(mOutStream!=null)
 				mOutStream.close();
@@ -191,18 +160,8 @@ public class ServerThead extends Thread {
 		 	}
 	        return b;
 	    }
-	 public static String Byte6ToMac(byte[] c) {
-	        return Integer.toHexString(((int)c[0]&0xff))+":"+Integer.toHexString(((int)c[1]&0xff))+":"+Integer.toHexString(((int)c[2]&0xff))+":"
-	        		+((short)c[3]&0xff)+":"+Integer.toHexString(((int)c[4]&0xff))+":"+Integer.toHexString(((int)c[5]&0xff)); 
-	    }
-	 public static String BytesToString(byte[] b,int size){
-		 PrintBytes(b,size);
-		 String str= "";
-		 for(int i=0;i<size;++i){
-			 str+=b[i]-'0';
-		 }
-		 return str;
-	 }
+
+
 	 public static void PrintBytes(byte[] c,int size){
 		 for(int i=0;i<size;++i)
 				System.out.print(Integer.toHexString(c[i]&0xff)+" ");

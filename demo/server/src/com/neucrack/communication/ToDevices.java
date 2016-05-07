fb691747f24a0878abf62b05eed9079f0ea8eaaa
Neucrack <CZD666666@gmail.com>
2016-05-07 19:34:18 +0800

    update interface of to devices

--- demo/server/src/com/neucrack/communication/ToDevices.java
+++ demo/server/src/com/neucrack/communication/ToDevices.java
@@ -7,8 +7,14 @@ public class ToDevices {
 	private DataInputStream mInStream=null;
 	private DataOutputStream mOutStream=null;
 	
-	public boolean WaitSignIn(String device,String userName,String passwd){
-		return false;
+	public ToDevices(DataInputStream inStream,DataOutputStream outStream){
+		mInStream = inStream;
+		mOutStream = outStream;
+	}
+	
+	public SignInfo WaitSignIn(){
+		SignInfo result = new SignInfo();
+		return result;
 	}
 	public boolean LightControl(String device,boolean isOn){
 		return isOn;
@@ -19,18 +25,44 @@ public class ToDevices {
 	public boolean DoorControl(String device,boolean isOn){
 		return isOn;
 	}
-	public boolean GetDoorStatus(String device,boolean isOn){
-		return isOn;
+	public DoorInfo GetDoorStatus(String device){
+		DoorInfo door = new DoorInfo();
+		return door;
 	}
 	
-	public boolean GetLightStatus(String device,boolean isOn){
-		return isOn;
+	public LightInfo GetLightStatus(String device){
+		LightInfo light = new LightInfo();
+		return light;
 	}
-	public boolean GetCurtainStatus(String device,boolean isOn){
-		return isOn;
+	public CurtainInfo GetCurtainStatus(String device){
+		CurtainInfo curtain = new CurtainInfo();
+		return curtain;
 	}
-	public boolean GetSensorData(String device,int SensorName,int value){
-		return false;
+	public SensorInfo GetSensorData(String device,int SensorName){
+		SensorInfo sensor = new SensorInfo();
+		return sensor;
 	}
 	
+	class SignInfo{
+		boolean isValid;
+		String device;
+		String userName;
+		String passwd;
+	}
+	class LightInfo{
+		boolean isValid;
+		boolean isOn;
+	}
+	class CurtainInfo{
+		boolean isValid;
+		boolean isOn;
+	}
+	class SensorInfo{
+		boolean isValid;
+		int value;
+	}
+	class DoorInfo{
+		boolean isValid;
+		boolean isOn;
+	}
 }

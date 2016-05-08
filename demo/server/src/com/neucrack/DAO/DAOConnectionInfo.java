87246475eded5d40fcfd764128aef59d42eb899a
Neucrack <CZD666666@gmail.com>
2016-05-08 20:21:19 +0800

    add DB connection info (empty)

---
+++ demo/server/src/com/neucrack/DAO/DAOConnectionInfo.java
@@ -0,0 +1,8 @@
+package com.neucrack.DAO;
+
+public class DAOConnectionInfo {
+	String mUrl = "https://leancloud.cn:443/1.1/users";
+	String X_LC_Id="";
+	String X_LC_Key="";
+	String Content_Type="application/json";
+}

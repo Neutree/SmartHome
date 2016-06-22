28eed405a487b30a96d73ba37193e03c353d3483
Neucrack <CZD666666@gmail.com>
2016-06-22 13:41:29 +0800

    final fix

--- demo/server/src/com/neucrack/DAO/DAOConnectionInfo.java
+++ demo/server/src/com/neucrack/DAO/DAOConnectionInfo.java
@@ -4,8 +4,8 @@ import com.neucrack.tool.Encrypt;
 
 public class DAOConnectionInfo {
 	public static String mUrl = "https://api.leancloud.cn/1.1/";
-	public static String X_LC_Id="";
-	public static String X_LC_Key="";
+	public static String X_LC_Id="Y79QEXdCKF4RTDqKqaFIdLH2-gzGzoHsz";
+	public static String X_LC_Key="nkc8xpxnhjR9KGCXJF5BaRkB";
 	public static String Content_Type="application/json";
 	
 	public static String adminName="15023490062";
